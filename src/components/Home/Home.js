6a771988a31039fc71528d349e49e3e44c14a208
Your Name <sanjida.ier@gmail.com>
2022-10-12 00:31:31 -0400

    home  commit

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,10 +6,18 @@ const Home = () => {
   const topics = useLoaderData();
   console.log(topics);
   return (
-    <div className="home-topics">
-      {topics.data.map((topic) => (
-        <Topic key={topic.id} topic={topic}></Topic>
-      ))}
+    <div>
+      <div className="home-pic">
+        <img
+          src="https://img.freepik.com/free-photo/intellectual-banknote-student-sand-computer-graphic_1134-1185.jpg?w=1060&t=st=1665548854~exp=1665549454~hmac=490fb631c2451ec73ea372e7015edad6cc56f090826c4dd8a57be7250b393f46"
+          alt=""
+        />
+      </div>
+      <div className="home-topics">
+        {topics.data.map((topic) => (
+          <Topic key={topic.id} topic={topic}></Topic>
+        ))}
+      </div>
     </div>
   );
 };
