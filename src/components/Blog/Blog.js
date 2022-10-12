e369c6eadc21e5c474e90723356936ff62f05620
Your Name <sanjida.ier@gmail.com>
2022-10-12 00:19:46 -0400

    blog commit

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -1,9 +1,29 @@
 import React from "react";
-
+import "./Blog.css";
 const Blog = () => {
   return (
-    <div>
-      <h1>this is blog</h1>
+    <div className="blog">
+      <h1> Blog questions</h1>
+      <h2>What is the purpose of react Router?</h2>
+      <p>
+        Answer--React Router is a standard library system built on top of the
+        React and used to create routing in the React application using React
+        Router Package.
+      </p>
+      <h2>How content API work?</h2>
+      <p>
+        An API, short for Application Programming Interface, is a
+        software-to-software interface. APIs provide a secure and standardized
+        way for applications to work with each other and deliver the information
+        or functionality requested without user intervention.
+      </p>
+      <h2>How does href work in react?</h2>
+      <p>
+        This href attribute contains the URL or path to the destination page. It
+        may be a relative URL or an absolute URL. In React, relative URLs should
+        always be handled by the link tag provided by the React Router, and pure
+        anchor tags should only be used for absolute paths.
+      </p>
     </div>
   );
 };
--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -4,8 +4,8 @@ const Quiz = ({ quiz }) => {
   const { correctAnswer, options, question } = quiz;
   return (
     <div className="quiz">
-      <h1 className="question">Question----{question}</h1>
-      <h3 className="option"> Options--{options}</h3>
+      <h1 className="question">Question----{question.slice(3, -4)}</h1>
+      <li className="option"> Options--{options}</li>
       <h4 className="correct">CorrectAns---{correctAnswer}</h4>
     </div>
   );
